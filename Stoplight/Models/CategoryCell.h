b206a5b350d902cc10b14a9da350d2a5789c5d3a
Ezi Godfreey-Igwe <arleneigwe@fb.com>
2019-07-18 11:30:29 -0700

    Creation of category cell object. Assigned cell on collection view as an article cell. Assigned collection view as a property of category cell.

---
+++ Stoplight/Models/CategoryCell.h
@@ -0,0 +1,18 @@
+//
+//  CategoryCell.h
+//  Stoplight
+//
+//  Created by arleneigwe on 7/18/19.
+//  Copyright © 2019 powercarlos25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CategoryCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UICollectionView *categoryCollectionView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Stoplight/Models/CategoryCell.m
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.m
+//  Stoplight
+//
+//  Created by arleneigwe on 7/18/19.
+//  Copyright © 2019 powercarlos25. All rights reserved.
+//
+
+#import "CategoryCell.h"
+
+@implementation CategoryCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
