1c58f03463ab27d40c3f86841ac9c321aeca9b73
Ezi Godfreey-Igwe <arleneigwe@fb.com>
2019-07-19 11:20:12 -0700

    Created settings view controller files and made outlets for its elements

---
+++ Stoplight/View Controllers/SettingsViewController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.h
+//  Stoplight
+//
+//  Created by arleneigwe on 7/19/19.
+//  Copyright © 2019 powercarlos25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
