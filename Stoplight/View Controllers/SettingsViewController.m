4ccc434617c7e404f52193fd3f5623af72ef60bf
Emily Hsiao <emily13hsiao@fb.com>
2019-07-22 16:21:35 -0700

    Set up saving user defaults in settings page, but have not set up ability to choose actual items yet. (Will need tableview set up.)

--- Stoplight/View Controllers/SettingsViewController.m
+++ Stoplight/View Controllers/SettingsViewController.m
@@ -16,7 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    //in future, probably won't be the same
+    //@property (strong, nonatomic) NSArray *categoriesList;
+    
+    //dictionary --> (key) pol aff. to (val) list of sources
+    NSSet *leftSet = [[NSSet alloc] initWithObjects:@"Vox", @"NBC", @"CNBC", @"The Economist", @"CNN", nil];
+    NSSet *centerSet = [[NSSet alloc] initWithObjects:@"WSJ", @"Bloomberg", @"Reuters", @"AP News", nil];
+    NSSet *rightSet = [[NSSet alloc] initWithObjects:@"Fox Business", "Fox News", "New York Post", nil];
+    NSDictionary *sourcesDict = [[NSDictionary alloc] initWithObjectsAndKeys:leftSet, "left", centerSet, "center", rightSet, "right", nil];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:sourcesDict forKey:@"sourcesDict"];
+    
+    NSArray *categoriesList = [[NSArray alloc] initWithObjects:@"politics", @"business", @"us", @"world", nil];
+    [defaults setObject:categoriesList forKey:@"categoriesArray"];
+    [defaults synchronize];
+    
+    
+    
 }
 
 /*
